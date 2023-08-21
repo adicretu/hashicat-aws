f0d292a75a02c59bf99f7a85a623e30ee67fbd62
Adrian Cretu <adicretu123@yahoo.co.uk>
2023-08-21 13:35:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "adicretu"
+ 
   bucket = "adicretu-s3-adhjhsg711212"
   acl    = "private"
 
